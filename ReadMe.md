2006c0127f05ee8c5e4c51f944a0a33665c96a3a
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-05-17 15:29:59 +0100

    package restructure

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
 ### Banking App
+
+Golang microservices Hexagonical implementation
--- main.go
+++ main.go
@@ -1,49 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name    string `json:"name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zip_code"`
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	sc := []Customer{
-		{
-			Name: "John Doe", City: "New York", Zipcode: "1100034",
-		},
-		{
-			Name: "Jane Doe", City: "Silicon Valley", Zipcode: "8900001",
-		},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(sc)
-	}
-
-	w.Header().Add("Content-Type", "application/json")
-	// encode struct to json
-	json.NewEncoder(w).Encode(sc)
-
-}
+import "github.com/lorezi/golang-bank-app/app"
 
 func main() {
-	// defining routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	// starting serve
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	app.Start()
 }
