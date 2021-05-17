4db6e43fd59531c40ad6d3f68061b1e9e2fcaf20
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-05-17 13:25:56 +0100

    fetch all customers endpoint

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string
+	City    string
+	Zipcode string
+}
+
 func greet(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(rw, "Hello World!")
+}
+
+func getAllCustomers(rw http.ResponseWriter, r *http.Request) {
+	sc := []Customer{
+		{
+			Name: "John Doe", City: "New York", Zipcode: "1100034",
+		},
+		{
+			Name: "Jane Doe", City: "Silicon Valley", Zipcode: "8900001",
+		},
+	}
+
+	json.NewEncoder(rw).Encode(sc)
 }
 
 func main() {
 	// defining routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting serve
 	log.Fatal(http.ListenAndServe(":8000", nil))
