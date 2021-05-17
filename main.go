e511cff18d9fb6ab83e3e8baa544d1234ad68f17
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-05-17 14:53:47 +0100

    xml marshalling

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +28,15 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	w.Header().Add("Content-Type", "application/json")
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(sc)
+	}
 
+	w.Header().Add("Content-Type", "application/json")
+	// encode struct to json
 	json.NewEncoder(w).Encode(sc)
+
 }
 
 func main() {
