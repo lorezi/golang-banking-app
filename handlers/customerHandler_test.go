1673e836690b2197fa6084d75e74d0ed23a75c45
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-01 15:28:17 +0100

    adds customer service mock

---
+++ handlers/customerHandler_test.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+	"github.com/lorezi/golang-bank-app/dto"
+	"github.com/lorezi/golang-bank-app/mocks"
+)
+
+func Test_should_return_customers_with_status_code_200(t *testing.T) {
+
+	// Arrange ---> set up mock services
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mck := mocks.NewMockCustomerService(ctrl)
+	dummyCustomers := []dto.CustomerResponse{
+		{
+			Name: "John Doe", City: "New York", Zipcode: "1100034", DateofBirth: "2000-01-04", Status: "true", Id: "100001",
+		},
+		{
+			Name: "Jane Doe", City: "Silicon Valley", Zipcode: "8900001", DateofBirth: "2000-01-08", Status: "false", Id: "100034",
+		},
+	}
+	mck.EXPECT().GetAllCustomers("").Return(dummyCustomers, nil)
+	ch := CustomerHandler{CustomerService: mck}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/customers", ch.GetAllCustomers)
+
+	req, _ := http.NewRequest(http.MethodGet, "/customers", nil)
+
+	// Act
+	rec := httptest.NewRecorder()
+	r.ServeHTTP(rec, req)
+
+	// Assert
+	if rec.Code != http.StatusOK {
+		t.Error("Failed while testing the status code")
+	}
+
+}
