f62867dff4484f1335459b6048416f005d52116d
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-01 12:36:48 +0100

    adds unit test for amount

--- dto/transactionRequest_test.go
+++ dto/transactionRequest_test.go
@@ -23,3 +23,20 @@ func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal
 		t.Error("Invalid http code while testing type")
 	}
 }
+
+func Test_should_return_error_when_amount_is_less_than_zero(t *testing.T) {
+	req := TransactionRequest{
+		TransactionType: DEPOSIT,
+		Amount:          -100,
+	}
+
+	appErr := req.Validate()
+
+	if appErr.Message != "amount cannot be a negative value" {
+		t.Error("Invalid message while testing type")
+	}
+
+	if appErr.Code != http.StatusUnprocessableEntity {
+		t.Error("Invalid http code while testing type")
+	}
+}
