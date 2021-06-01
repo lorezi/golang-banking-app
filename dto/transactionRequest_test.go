cf62b18421f2a824ef154988a6ac4b80205fdf10
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-01 12:11:51 +0100

    adds validate error code for transaction request test

--- dto/transactionRequest_test.go
+++ dto/transactionRequest_test.go
@@ -1,6 +1,9 @@
 package dto
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal(t *testing.T) {
 	// Arrange
@@ -11,8 +14,12 @@ func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal
 	// Act
 	appErr := req.Validate()
 
-	// Assert
+	// Assert for invalid message and code
 	if appErr.Message != "The transaction type must be withdrawal or deposit" {
 		t.Error("Invalid message while testing type")
+	}
+
+	if appErr.Code != http.StatusUnprocessableEntity {
+		t.Error("Invalid http code while testing type")
 	}
 }
