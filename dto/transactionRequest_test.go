0163f02cff917f130856b3b5bb759de506de6641
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-02 10:49:10 +0100

    refactors test suites

--- dto/transactionRequest_test.go
+++ dto/transactionRequest_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal(t *testing.T) {
+func Test_should_return_error_when_transaction_request_type_is_not_deposit_or_withdrawal(t *testing.T) {
 	// Arrange
 	req := TransactionRequest{
 		TransactionType: "invalid transaction type",
@@ -16,27 +16,30 @@ func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal
 
 	// Assert for invalid message and code
 	if appErr.Message != "The transaction type must be withdrawal or deposit" {
-		t.Error("Invalid message while testing type")
+		t.Error("Invalid message while testing for transaction request type")
 	}
 
 	if appErr.Code != http.StatusUnprocessableEntity {
-		t.Error("Invalid http code while testing type")
+		t.Error("Invalid http code while testing for transaction request type")
 	}
 }
 
 func Test_should_return_error_when_amount_is_less_than_zero(t *testing.T) {
+	// Arrange
 	req := TransactionRequest{
 		TransactionType: DEPOSIT,
 		Amount:          -100,
 	}
 
+	// Act
 	appErr := req.Validate()
 
 	if appErr.Message != "amount cannot be a negative value" {
-		t.Error("Invalid message while testing type")
+		t.Error("Invalid message while testing for amount negative value")
 	}
 
+	// Assert
 	if appErr.Code != http.StatusUnprocessableEntity {
-		t.Error("Invalid http code while testing type")
+		t.Error("Invalid message while testing for amount negative value")
 	}
 }
